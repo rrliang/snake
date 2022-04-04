ca9f318b139bf2872ebede9bfc96a8a398b30ff1
rrliang <42505429+rrliang@users.noreply.github.com>
2022-04-03 20:09:25 -0400

    Update mysnake.c

--- mysnake.c
+++ mysnake.c
@@ -3,6 +3,8 @@
 #include <ncurses.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <string.h>
+// #include <time.h>
 
 #define MAIN_WIN_COLOR 1
 #define DELAY 100000
@@ -10,15 +12,24 @@
 void initboard();
 char* initialize(int C);
 void printboard(char c, WINDOW *win);
+void printsnakebod();
+bool didsnakehitself();
+unsigned int rand_interval(unsigned int min, unsigned int max);
+struct snake;
 int **boardArr;
+int *snakebodyi;
+int *snakebodyj;
 int maxrow, maxcol;
 int snakesize = 5;
 int currenti = 1;
-int currentj = 1;
+int currentj = 5;
 bool running = true;
 char* endingmsg;
+int trophyi, trophyj;
+int trophyval;
+int winningper;
 
-int kbhit()
+int kbhit() //https://stackoverflow.com/questions/448944/c-non-blocking-keyboard-input
 {
     struct timeval tv = { 0L, 0L };
     fd_set fds;
@@ -27,6 +38,67 @@ int kbhit()
     return select(1, &fds, NULL, NULL, &tv) > 0;
 }
 
+void trophygen(WINDOW *win)
+{
+    werase(win);
+    trophyval = rand_interval(0, 9);
+    
+    trophyi = rand_interval(1, maxrow);
+    trophyj = rand_interval(1, maxcol);
+    bool inotsame = false;
+    bool jnotsame = false;
+    while (inotsame) {
+        bool isamecurrent = (trophyi != currenti);
+        bool inotsamebody = true;
+        for (int i = 0; i < snakesize-1 ; i++) {
+            if (trophyi == snakebodyi[i]) {
+                inotsamebody = false;
+                break;
+            }
+        }
+        if (inotsame && inotsamebody) {
+            inotsame = true;
+        } else {
+            trophyi = rand_interval(1, maxrow);
+        }
+    }
+    while (jnotsame) {
+        bool jsamecurrent = (trophyj != currentj);
+        bool jnotsamebody = true;
+        for (int i = 0; i < snakesize-1 ; i++) {
+            if (trophyj == snakebodyj[i]) {
+                jnotsamebody = false;
+                break;
+            }
+        }
+        if (inotsame && jnotsamebody) {
+            jnotsame = true;
+        } else {
+            trophyj = rand_interval(1, maxcol);
+        }
+    }
+    
+    mvprintw(trophyi, trophyj,"%d",trophyval);
+    usleep(900000);
+}
+
+unsigned int rand_interval(unsigned int min, unsigned int max) //https://stackoverflow.com/questions/2509679/how-to-generate-a-random-integer-number-from-within-a-range
+{
+    int r;
+    const unsigned int range = 1 + max - min;
+    const unsigned int buckets = RAND_MAX / range;
+    const unsigned int limit = buckets * range;
+
+    /* Create equal size buckets all in a row, then fire randomly towards
+     * the buckets until you land in one of them. All buckets are equally
+     * likely. If you land off the end of the line of buckets, try again. */
+    do
+    {
+        r = rand();
+    } while (r >= limit);
+
+    return min + (r / buckets);
+}
 
 int main()
 {
@@ -48,18 +120,30 @@ int main()
     /*  Draw border  */
     initboard();
     //box(win, 0, 0);
-    int inputChar=KEY_RIGHT;
+    int inputChar, previousChar = KEY_RIGHT;
     int i , y = 0;
+    bool ranOnce = true;
+    int counter = 0;
     while (1) {
+        //struct snake snakebody[snakesize];
         werase(win);
         box(win, 0, 0);
         if (currenti == 0 || currentj == 0 || currenti == maxrow || currentj == maxcol) {
             endingmsg = "YOU LOST BECAUSE YOU RAN INTO THE BORDER!";
             break;
         }
+        int previousi = currenti;
+        int previousj = currentj;
         mvprintw(currenti, currentj, initialize(inputChar));
+        
+        
+        //trophygen(win);
+        refresh();
+            
+        
+        
         if (kbhit()) {
-            int previousChar = inputChar;
+            previousChar = inputChar;
             inputChar = getch();
             if ((previousChar == KEY_RIGHT && inputChar == KEY_LEFT) || (previousChar == KEY_LEFT && inputChar == KEY_RIGHT) ||
                 (previousChar == KEY_DOWN && inputChar == KEY_UP) || (previousChar == KEY_UP && inputChar == KEY_DOWN)) {
@@ -69,6 +153,25 @@ int main()
             refresh();
         } else {
             refresh();
+        }
+               
+        if (counter == (snakesize-2)) {
+            for(i=0; i<(snakesize-1); i++)
+            {
+                snakebodyi[i]=snakebodyi[i+1];
+                snakebodyj[i]=snakebodyj[i+1];
+            }
+            snakebodyi[(snakesize-1)] = previousi;
+            snakebodyj[(snakesize-1)] = previousj;
+        } else {
+            snakebodyi[counter] = previousi;
+            snakebodyj[counter] = previousj;
+            counter++;
+        }
+        printsnakebod();
+        if (didsnakehitself()) {
+            endingmsg = "YOU LOST BECAUSE YOU HIT YOURSELF!";
+            break;
         }
         refresh();
         // wrefresh(win);
@@ -94,6 +197,23 @@ int main()
     
 
     return EXIT_SUCCESS;
+}
+
+void printsnakebod() {
+    for (int i = 0; i < (snakesize-1); i++) {
+        if (snakebodyi[i] != 0 && snakebodyj[i] != 0) {
+            mvprintw(snakebodyi[i], snakebodyj[i], "o");
+        }
+    }
+}
+
+bool didsnakehitself(int currenti, int currentj) {
+    for (int i = 0; i < (snakesize-1); i++) {
+        if (snakebodyi[i] == currenti && snakebodyj[i] == currentj) {
+            return true;
+        }
+    }
+    return false;
 }
 
 char* initialize(int inputChar) {
@@ -189,6 +309,16 @@ void initboard() {
         if (boardArr[i] == NULL)
             return;
     }
+
+    snakebodyi = malloc((snakesize-1)*sizeof(int *));
+    if (snakebodyi == NULL)
+        return;
+    
+    snakebodyj = malloc((snakesize-1)*sizeof(int *));
+    if (snakebodyi == NULL)
+        return;
+
+    
 }
 
 void printboard(char c, WINDOW *win) {
@@ -211,6 +341,12 @@ struct snakehead {
     //hold the i and j of the head of the snake
     //hold the size of the snake
 
+};
+
+struct snake 
+{
+    int i;
+    int j;
 };
 
 struct trophy {
